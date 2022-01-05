c34b12b944dc93f8db53faddf0014f7fe6eed002
Chris Vanndy <2736@holbertonschool.com>
2022-01-05 14:06:23 -0600

    adding final touches to challenge

--- Assets/Scripts/ProjectorController.cs
+++ Assets/Scripts/ProjectorController.cs
@@ -6,10 +6,15 @@ public class ProjectorController : MonoBehaviour
 {
     public GameObject particles;
     public GameObject light;
+    public AudioSource congrats;
+    public AudioSource whoosh;
    
    public void Particles()
    {
        particles.SetActive(true);
        light.SetActive(true);
+       whoosh.Play();
+       congrats.Play();
+
    }
 }
